f4aadaf8f65360d7b7867060788ea96581331992
Ray <jbieaa@connect.ust.hk>
2024-07-24 10:47:09 +0800

    Init commit

---
+++ src/interview_150/Solution.java
@@ -0,0 +1,186 @@
+package interview_150;
+
+import java.util.*;
+
+public class Solution {
+    public int removeDuplicates(int[] nums) {
+        int re = 0;
+        if (nums.length == 1 || nums.length == 2) {
+            return nums.length;
+        }
+        int curr = nums[0];
+        int c_index = 0;
+        re++;
+        for (int i = 1; i <= nums.length-1; i++) {
+            if (nums[i] != curr) {
+                re++;
+                curr = nums[i];
+                c_index = i;
+            } else {
+                if (i == 1) {
+                    re++;
+                } else {
+                    if (i-c_index < 2) {
+                        re++;
+                    } else {
+                        nums[i] = 100000;
+                    }
+                }
+            }
+        }
+        for (int j = 1; j <= nums.length-2; j++) {
+            if (nums[j] == 100000) {
+                int flag = 0;
+                for (int k = j+1; k <= nums.length-1; k++) {
+                    if (nums[k] != 100000) {
+                        nums[j] = nums[k];
+                        nums[k] = 100000;
+                        flag = 1;
+                        break;
+                    }
+                }
+                if (flag == 0) {
+                    break;
+                }
+            }
+        }
+        return re;
+    }
+
+    public int majorityElement(int[] nums) {
+        if (nums.length <= 2) {
+            return nums[0];
+        }
+        int threshold = nums.length/2;
+        int flag = 0;
+        int re = nums[0];
+        for (int i = 0; i <= nums.length-1; i++) {
+            int curr = nums[i];
+            int count = 0;
+            for (int j = 0; j <= nums.length-1; j++) {
+                if (nums[j] == curr) {
+                    count++;
+                    if (count > threshold) {
+                        flag = 1;
+                        re = curr;
+                        break;
+                    }
+                }
+            }
+            if (flag == 1) {
+                break;
+            }
+        }
+        return re;
+    }
+
+    public void rotate(int[] nums, int k) {
+        if (k > 0) {
+            k = k % nums.length;
+            HashMap<Integer, Integer> map = new HashMap<>();
+            for (int i = 0; i <= nums.length - 1; i++) {
+                int next = i + k;
+                if (next > nums.length - 1) {
+                    next = next % nums.length;
+                }
+                map.put(next, nums[i]);
+            }
+            for (int j = 0; j <= nums.length - 1; j++) {
+                nums[j] = map.get(j);
+            }
+        }
+    }
+
+    public int maxProfit(int[] prices) {
+        if (prices.length == 1) {
+            return 0;
+        }
+        int maxProfit = 0;
+        int minPrice = prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            } else {
+                int profit = prices[i] - minPrice;
+                if (profit > maxProfit) {
+                    maxProfit = profit;
+                }
+            }
+        }
+        return maxProfit;
+    }
+
+    public int maxProfit2(int[] prices) {
+        if (prices.length == 1) {
+            return 0;
+        }
+        int maxProfit = 0;
+        int minPrice = prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] <= minPrice) {
+                minPrice = prices[i];
+            } else {
+                int profit = prices[i] - minPrice;
+                maxProfit += profit;
+                if (i != prices.length-1) {
+                    minPrice = prices[i];
+                }
+            }
+        }
+        return maxProfit;
+    }
+
+    public boolean canJump(int[] nums) {
+        int maxReach = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i > maxReach) {
+                return false;
+            }
+            maxReach = Math.max(maxReach, i + nums[i]);
+            if (maxReach >= nums.length - 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int jump(int[] nums) {
+        int count = 0;
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(nums.length-1);
+        while (!list.isEmpty()) {
+            int pos = list.removeFirst();
+            for (int i = 0; i <= nums.length-2; i++) {
+                int reach = i + nums[i];
+                if (reach >= pos) {
+                    count++;
+                    list.add(i);
+                    if (i == 0) {
+                        list.clear();
+                    }
+                    break;
+                }
+            }
+        }
+        return count;
+    }
+
+    public int hIndex(int[] citations) {
+        int index = 0;
+        while (true){
+            int count = 0;
+            for (int i = 0; i <= citations.length-1; i++) {
+                if (citations[i] >= index) {
+                    count++;
+                }
+            }
+            if (count < index) {
+                index--;
+                break;
+            } else {
+                index++;
+            }
+        }
+        return index;
+    }
+}
---
+++ src/interview_150/SolutionTest.java
@@ -0,0 +1,145 @@
+package interview_150;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+    @Test
+    public void testSingleElementArray() {
+        int[] nums = {1};
+        int result = new Solution().removeDuplicates(nums);
+        assertEquals(1, result);
+    }
+
+    @Test
+    public void testTwoElementArray() {
+        int[] nums = {1, 2};
+        int result = new Solution().removeDuplicates(nums);
+        assertEquals(2, result);
+    }
+
+    @Test
+    public void testNoDuplicates() {
+        int[] nums = {1, 2, 3, 4, 5};
+        int result = new Solution().removeDuplicates(nums);
+        assertEquals(5, result);
+    }
+
+    @Test
+    public void testAllDuplicates() {
+        int[] nums = {1, 1, 1, 1, 1};
+        int result = new Solution().removeDuplicates(nums);
+        assertEquals(2, result); // Only one duplicate allowed
+    }
+
+    @Test
+    public void testMixedDuplicates() {
+        int[] nums = {1, 1, 2, 2, 3, 3, 4, 4, 5, 5};
+        int result = new Solution().removeDuplicates(nums);
+        assertEquals(10, result); // Each number appears twice
+    }
+
+    @Test
+    public void testMoreThanTwoDuplicates() {
+        int[] nums = {1, 1, 1, 2, 2, 2, 3, 3, 3};
+        int result = new Solution().removeDuplicates(nums);
+        assertEquals(6, result); // Only two of each number allowed
+    }
+
+    @Test
+    public void testAlternatingNumbers() {
+        int[] nums = {1, 2, 1, 2, 1, 2};
+        int result = new Solution().removeDuplicates(nums);
+        assertEquals(6, result); // Each number appears twice
+    }
+
+    @Test
+    public void testSingleDuplicate() {
+        int[] nums = {1, 1, 2};
+        int result = new Solution().removeDuplicates(nums);
+        assertEquals(3, result); // Only one duplicate allowed
+    }
+
+    @Test
+    void majorTest() {
+        int[] sequence = new int[] {2, 2, 1, 1, 1, 2, 2};
+        Solution solution = new Solution();
+        assertEquals(2, solution.majorityElement(sequence));
+        int[] seq2 = new int[] {1};
+        assertEquals(1, solution.majorityElement(seq2));
+    }
+
+    @Test
+    void rotateTest() {
+        int[] sequence = new int[] {1,2,3,4,5,6,7};
+        int[] result = new int[] {5,6,7,1,2,3,4};
+        Solution solution = new Solution();
+        solution.rotate(sequence, 3);
+        for (int i = 0; i <= sequence.length-1; i++) {
+            assertEquals(result[i], sequence[i]);
+        }
+    }
+
+    @Test
+    public void testRotateArray() {
+        Solution rotateArray = new Solution();
+        int[] nums = {1, 2, 3, 4, 5};
+        int k = 2;
+        int[] expected = {4, 5, 1, 2, 3};
+        rotateArray.rotate(nums, k);
+        assertArrayEquals(expected, nums);
+        int[] n2 = {-33, 22, 6677};
+        k = 0;
+        expected = new int[] {-33, 22, 6677};
+        rotateArray.rotate(n2, k);
+        assertArrayEquals(expected, n2);
+    }
+
+    @Test
+    public void testSell() {
+        Solution solution = new Solution();
+        int[] prices = new int[] {7, 1, 5, 3, 6, 4};
+        assertEquals(5, solution.maxProfit(prices));
+        int[] p2 = new int[] {100};
+        assertEquals(0, solution.maxProfit(p2));
+    }
+
+    @Test
+    public void testSell2() {
+        Solution solution = new Solution();
+        int[] prices = new int[] {7, 1, 5, 3, 6, 4};
+        assertEquals(7, solution.maxProfit2(prices));
+        int[] p2 = new int[] {1, 2, 3, 4, 5};
+        assertEquals(4, solution.maxProfit2(p2));
+        int[] p3 = new int[] {115};
+        assertEquals(0, solution.maxProfit2(p3));
+    }
+
+    @Test
+    public void testJump() {
+        Solution solution = new Solution();
+        int[] p1 = new int[] {0};
+        assertTrue(solution.canJump(p1));
+        int[] p2 = new int[10000];
+        p2[0] = 10000;
+        assertTrue(solution.canJump(p2));
+        int[] p3 = new int[] {4, 3, 2, 1, 0, 100, 3};
+        assertFalse(solution.canJump(p3));
+    }
+
+    @Test
+    public void testJump2() {
+        Solution solution = new Solution();
+        int[] p1 = new int[] {2, 3, 0, 1, 4};
+        assertEquals(2, solution.jump(p1));
+        int[] p2 = new int[] {2, 3, 1, 1, 4};
+        assertEquals(2, solution.jump(p2));
+    }
+
+    @Test
+    public void testHIndex() {
+        Solution solution = new Solution();
+        int[] p1 = new int[] {3, 0, 6, 1, 5};
+        assertEquals(3, solution.hIndex(p1));
+    }
+}
