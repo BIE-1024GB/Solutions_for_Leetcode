3c4b80318d3067410d00eb87dcd819dd475a875e
Ray <jbieaa@connect.ust.hk>
2024-10-17 16:59:01 +0800

    Changed lambda expressions

--- src/interview_150/Solution.java
+++ src/interview_150/Solution.java
@@ -1394,7 +1394,7 @@ public class Solution {
 
     public int[][] merge(int[][] intervals) {
         // Sort intervals by the start time
-        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        Arrays.sort(intervals, Comparator.comparingInt(a -> a[0]));
         LinkedList<int[]> merged = new LinkedList<>();
         for (int[] interval : intervals) {
             // If the list of merged intervals is empty or if the current interval does not overlap with the previous,
@@ -1434,7 +1434,7 @@ public class Solution {
         //                  keep track of the "end" variable: update when a new arrow is needed(no overlapping).
         if (points.length == 1)
             return 1;
-        Arrays.sort(points, (a, b) -> Integer.compare(a[1], b[1]));
+        Arrays.sort(points, Comparator.comparingInt(a -> a[1]));
         int arrows = 1;
         int end = points[0][1];
         for (int i = 1; i <= points.length-1; i++) {
