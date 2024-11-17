5e66a90ccef652e8635d2d2d91d843b8b96173a8
Ray <jbieaa@connect.ust.hk>
2024-11-17 16:43:20 +0800

    Adjustments

--- src/interview_150/Solution.java
+++ src/interview_150/Solution.java
@@ -2064,7 +2064,7 @@ public class Solution {
         public boolean isSameTree(TreeNode p, TreeNode q) {
             if (p == null && q == null) {
                 return true;
-            } else if (p == null && q != null || p != null && q == null) {
+            } else if (p == null || q == null) {
                 return false;
             }
             if (p.val != q.val) {
@@ -2086,7 +2086,7 @@ public class Solution {
         private boolean mirror(TreeNode p, TreeNode q) {
             if (p == null && q == null) {
                 return true;
-            } else if (p == null && q != null || p != null && q == null) {
+            } else if (p == null || q == null) {
                 return false;
             }
             if (p.val != q.val) {
