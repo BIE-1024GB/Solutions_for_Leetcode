38e0ccbe97047c24f65b866120aa5c761bf40ef0
Ray <jbieaa@connect.ust.hk>
2025-08-15 21:25:17 -0700

    Updates

--- src/top_100/Solution.java
+++ src/top_100/Solution.java
@@ -1717,6 +1717,55 @@ public class Solution {
             }
             return null;
         }
+
+        public ListNode reverseList(ListNode head) {
+            // Optimal recursion method
+            if (head == null || head.next == null) {
+                return head;
+            } else {
+                ListNode nh = reverseList(head.next);
+                head.next.next = head;
+                head.next = null;
+                return nh;
+            }
+        }
+
+        private ListNode reverseListII(ListNode head) {
+            ListNode prev = null;
+            ListNode curr = head;
+            while (curr != null) {
+                ListNode nextTemp = curr.next;
+                curr.next = prev;
+                prev = curr;
+                curr = nextTemp;
+            }
+            return prev;
+        }
+        public boolean isPalindrome(ListNode head) {
+            if (head.next == null)
+                return true;
+            // Step 1: Find middle using slow and fast pointers
+            ListNode slow = head, fast = head;
+            while (fast != null && fast.next != null) {
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+            // Step 2: Reverse second half
+            ListNode secondHalfHead = reverseListII(slow);
+            // Step 3: Compare both halves
+            ListNode p1 = head;
+            ListNode p2 = secondHalfHead;
+            boolean result = true;
+            while (p2 != null) {
+                if (p1.val != p2.val) {
+                    result = false;
+                    break;
+                }
+                p1 = p1.next;
+                p2 = p2.next;
+            }
+            return result;
+        }
     }
 
     static class Node {
