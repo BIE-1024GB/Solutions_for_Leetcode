b3598ef5a474863b0ed7af0f37d5d2d53fe6d245
Ray <jbieaa@connect.ust.hk>
2026-02-16 17:28:53 -0800

    Updates

--- src/miscellaneous/Solution.java
+++ src/miscellaneous/Solution.java
@@ -4422,9 +4422,24 @@ public class Solution {
         int res = 0;
         for (int i = 1; i <= 31; i++) {
             int lsb = n&1;
-            res += lsb*Math.pow(2, 32-i);
+            res = res|lsb;
+            res = res<<1;
             n = n>>1;
         }
         return res;
+    }
+
+    public List<String> readBinaryWatch(int turnedOn) {
+        List<String> result = new ArrayList<>();
+        for (int hour = 0; hour < 12; hour++) {
+            for (int minute = 0; minute < 60; minute++) {
+                // Count total 1-bits in hour and minute
+                if (Integer.bitCount(hour) + Integer.bitCount(minute) == turnedOn) {
+                    String time = hour + ":" + (minute < 10 ? "0" + minute : minute);
+                    result.add(time);
+                }
+            }
+        }
+        return result;
     }
 }
