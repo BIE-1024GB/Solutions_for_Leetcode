f63e770578633a9669ac84abdd065ba0d53df3c6
Ray <jbieaa@connect.ust.hk>
2026-03-14 20:07:55 -0700

    Updates

--- src/miscellaneous/Solution.java
+++ src/miscellaneous/Solution.java
@@ -5115,4 +5115,52 @@ public class Solution {
             return list.get(k-1);
         }
     }
+
+    static class Fancy {
+        private final List<Long> list;
+        private long mul = 1;
+        private long add = 0;
+        private final int mod = 1000000007;
+
+        public Fancy() {
+            list = new ArrayList<>();
+        }
+
+        public void append(int val) {
+            long adjusted = ((val - add + mod) % mod) * modInverse(mul) % mod;
+            list.add(adjusted);
+        }
+
+        public void addAll(int inc) {
+            add = (add + inc) % mod;
+        }
+
+        public void multAll(int m) {
+            mul = (mul * m) % mod;
+            add = (add * m) % mod;
+        }
+
+        public int getIndex(int idx) {
+            if (idx >= list.size()) return -1;
+            long val = list.get(idx);
+            return (int)((val * mul + add) % mod);
+        }
+
+        private long modInverse(long x) {
+            return pow(x, mod - 2);
+        }
+
+        private long pow(long a, long b) {
+            long res = 1;
+            a %= mod;
+
+            while (b > 0) {
+                if ((b & 1) == 1)
+                    res = res * a % mod;
+                a = a * a % mod;
+                b >>= 1;
+            }
+            return res;
+        }
+    }
 }
